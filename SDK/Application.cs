663bdb62da734bb1cd392824e18252ec296465df
ocean <3408598@qq.com>
2019-08-31 09:25:21 +0800

    add

---
+++ SDK/Application.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ocean.sdk.error;
+using ocean.sdk.entity;
+using ocean.sdk.utils;
+using Newtonsoft.Json;
+
+namespace ocean.sdk
+{
+    class Application
+    {
+        public string NodeUrl { get; }
+
+        public Application(string nodeUrl)
+        {
+            this.NodeUrl = nodeUrl;
+        }
+
+        public ApplicationResp FindAppInfo(string address,string id)
+        {
+            string url = string.Format("{0}/manager/app/findAppInfo?id={1}", NodeUrl, id);
+            return this.CallChainAPI<ApplicationResp>(url);
+        }
+
+        private T CallChainAPI<T>(string url)
+        {
+            string json = HttpHelper.Get(url);
+            return ParseResponse<T>(json);
+        }
+
+        private T CallChainAPI<T>(string url, string jsonData)
+        {
+            string json = HttpHelper.Post(url, jsonData);
+            return ParseResponse<T>(json);
+        }
+
+        private T ParseResponse<T>(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception)
+            {
+                throw ApiError.FromJson(json);
+            }
+        }
+    }
+}
---
+++ SDK/entity/ApplicationResp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ocean.sdk.entity
+{
+    class ApplicationResp
+    {
+        public string message { get; set; }
+
+        public string data { get; set; }
+
+        public int    status { get; set; }
+
+        public string errorCode { get; set; }
+
+        public string errorMessage { get; set; }
+
+        public string exception { get; set; }
+    }
+}
