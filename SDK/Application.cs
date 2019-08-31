46d0c06faa88b1c0ef8c32ca7cd3f20411c11d63
ocean <3408598@qq.com>
2019-08-31 10:58:10 +0800

    error

--- SDK/Application.cs
+++ SDK/Application.cs
@@ -43,7 +43,7 @@ namespace ocean.sdk
             {
                 return JsonConvert.DeserializeObject<T>(json);
             }
-            catch (Exception e)
+            catch (Exception)
             {
                 throw ApiError.FromJson(json);
             }
--- SDKTests/ApplicationTests.cs
+++ SDKTests/ApplicationTests.cs
@@ -17,7 +17,8 @@ namespace ocean.sdk.Tests
         {
             Application app = new Application("http://172.16.2.51:2500/");
             ApplicationResp resp = app.FindAppInfo("1");
-            Assert.Fail();
+            Assert.AreEqual("1", resp.data.list[0].id, "id error");
+            //Assert.Fail();
         }
     }
 }
